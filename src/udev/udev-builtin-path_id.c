7ffd8d0a3865f2391c9d50d6bc654b36b7e0fd01
Yu Watanabe <watanabe.yu+github@gmail.com>
2022-10-10 14:20:33 +0900

    udev: drop assertion which is always false
    Fixes a bug introduced by 67c3e1f63a5221b47a8fea85ae421671f29f3b7e.
    
    Fixes #24945.
    
    (cherry picked from commit 6209bbbd4b1c9ed2886028ab2ee3df0a7d0e2494)
    
    Related: #2102515

--- src/udev/udev-builtin-path_id.c
+++ src/udev/udev-builtin-path_id.c
@@ -552,7 +552,6 @@ static int find_real_nvme_parent(struct udev_device *dev, struct udev_device **r
          *   /sys/devices/pci0000:00/0000:00:1c.4/0000:3c:00.0/nvme/nvme0 */
 
         assert(dev);
-        assert(nvme);
 
         sysname = udev_device_get_sysname(dev);
         if (!sysname)
